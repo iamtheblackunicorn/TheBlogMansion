dab7e9eb82b0d3df1bdbce8d99a4152f7d20edaf
iamtheblackunicorn <drkuncrn@protonmail.ch>
2021-07-10 16:56:05 +0200

    Tests are missing.

--- files/views.py
+++ files/views.py
@@ -7,7 +7,9 @@ def download(request,authCode):
     file = UserFile.objects.get(assetAuthCode=authCode)
     alias = file.alias
     url = file.userAsset.url
-    return render(request, 'files/download.html', {'url': url, 'alias':alias})
+    path = file.userAsset.url.split('/')
+    file = path[-1]
+    return render(request, 'files/download.html', {'url': url, 'alias':alias, 'file': file})
 def authorize(request):
     if request.method == 'POST':
         form = AuthForm(request.POST)
@@ -15,8 +17,9 @@ def authorize(request):
             code = form.cleaned_data['code']
             userFile = UserFile.objects.get(assetAuthCode=code)
             actualCode = userFile.assetAuthCode
-            #1234567892356894
             return redirect('files:download', authCode=actualCode)
+        else:
+            print("Error!")
     else:
         form = AuthForm()
     return render(request, 'files/form.html', {'form': form})
